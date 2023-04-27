03ae029c88b136c4115ce1fd299b67664f8bc0af
zmabel4 <s.mdzebele@lab.clicklearning.org>
2023-04-28 00:17:37 +0200

    My IWA_2

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// This is the firstname of the user
+
+const user = "John";
+
+// This is is the surname of the user
+
+
+const surname = "Smith";
+console.log(user + surname);
+
+// (c) ACM Inc. 2010
+
+// This is the date that a user creared their account
+
+const date = "10/07/2014";
+
+console.log(date);
+
+ 
+// It is important to show the following massage in order to ;
+//-to assure users 
+ //-to assure hackers
+ //-to impress investores
+
+ console.log("security scan starting");
