178c852911285e7aa00a421bbf2aab141cda1ed2
sifiso mdzebele <127992943+zmabel4@users.noreply.github.com>
2023-05-07 16:01:35 +0200

    Add files via upload

---
+++ IWA_5/scripts.js
@@ -0,0 +1,35 @@
+let freeWarning = 'Free shipping only applies to single customer orders';
+let bannedWarning = 'Unfortunately we do not ship to North Korea';
+let price = '1270';
+let Rand = 'currency'
+
+console.log(freeWarning, bannedWarning);
+
+
+
+if(price <= 1270) {
+    console.log(price, 'Shipping');
+    console.log(Rand, 'RAND');
+}
+const select = (3 === 3 && 3 === 3);
+console.log(select);
+
+const shoes = 300 * 1
+const toys = 100 * 5
+const shirts = 500 * 'NONE_SELECTED'
+const batteries = 35 * 2
+const pens = 5 * 'NONE_SELECTED'
+const selected = 3
+
+console.log(shoes);
+console.log(toys);
+console.log(shirts);
+console.log(batteries);
+console.log(pens);
+console.log(selected);
+
+const customer = 1
+const country = "RSA"
+
+console.log(customer,);
+console.log(country);
