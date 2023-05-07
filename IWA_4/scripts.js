f66fb30cdcbed199cc790d9efcedc70e326293a4
sifiso mdzebele <127992943+zmabel4@users.noreply.github.com>
2023-05-07 11:45:23 +0200

    Add files via upload

---
+++ IWA_4/scripts.js
@@ -0,0 +1,19 @@
+// final output 
+
+const year = "2050";
+const status = "parent";
+const count = "9";
+
+if (year <= 2050) {
+    console.log(year, 'Christmas Day');
+    console.log(year, 'New Years Day');
+    console.log(year, 'Human Rights Day');
+    console.log(year, 'Family Day');
+    console.log(year, 'Freedom Day');
+    console.log(year, 'Womens Day');
+    console.log(year, 'Heritage Day');
+    console.log(year, 'Day of Condiliation');
+    console.log(year, 'Day of Goodwill');
+    console.log(count);
+
+}
