b8261f189c964b47f69694a12848a304ed016580
sifiso mdzebele <127992943+zmabel4@users.noreply.github.com>
2023-06-10 15:43:19 +0200

    Add files via upload

---
+++ IWA_14/ChaLENGE_2/scripts.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function internal() {
+  const added = add(this.internal.a, this.internal.b);
+  const multiplied = multiply(added, this.internal.c);
+  
+  console.log(multiplied);
+  return this;
+}
+
+// Not allowed to change below this
+
+const example1 = {
+  internal: {
+    a: 2,
+    b: 4,
+    c: 8,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
+
+const example2 = {
+  internal: {
+    a: 2,
+    b: 2,
+    c: 3,
+  },
+  add,
+  multiply,
+  calculate: internal,
+};
+
+example1.calculate();
+example2.calculate();
